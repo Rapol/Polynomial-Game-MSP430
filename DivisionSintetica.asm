3516275e3bf45acad1d20caa6443a603f8deb22b
Rafael Pol <rafael.pol1@upr.edu>
2013-05-13 23:02:40 -0400

    Commit 2

--- DivisionSintetica.asm
+++ DivisionSintetica.asm
@@ -1,7 +1,7 @@
 #include "msp430.h"	
-Delay2	MACRO
+Delay2	MACRO	dato
 	LOCAL	Loop1
-	mov.w	#50000,R10
+	mov.w	dato,R10
 Loop1:	dec	R10
 	nop
 	nop
@@ -59,102 +59,101 @@ Return:	ENDM
 ;---------------------------------------------------------------
 		ORG	0F800h			; Program Start
 ;---------------------------------------------------------------
-P0		DW	25,-2,102,5
-X0		DW	12
-P1		DW	-9,6,-13,15
-X1		DW	-10
-P2		DW	1,25,-6,7
-X2		DW	-15
-P3		DW	4,106,-110,87
-X3		DW	-30
-		EVEN
-RESET		mov.w	#0280h,SP		; Initialize stackpointer
+P0		DW	25,-2,102,5		; P0 Polynomial
+X0		DW	12			; X0 Value
+P1		DW	-9,6,-13,15		; P1 Polynomial
+X1		DW	-10			; X1 Value
+P2		DW	1,25,-6,7		; P2 Polynomial
+X2		DW	-15			; X2 Value
+P3		DW	4,106,-110,87		; P3 Polynomial
+X3		DW	-30			; X3 Value
+		EVEN				; To ensure that addresses are even
+RESET		mov.w	#0280h,SP		; Initialize Stack-Pointer
 StopWDT 	mov.w	#WDTPW+WDTHOLD,&WDTCTL	; StopWDT
-		bic.b	#11000000b,P2SEL	; Selecting pins for output ////////NO SE SI ESTO ESTA CORRECTO/////////////
+		bic.b	#11000000b,P2SEL	; Selecting pins for output
+						; P2.7 and P2.6, their default operation is set for module operation.
 		bis.b	#11111111b,P2DIR	; P2.0-P2.7 as output ports
-LOW		bic.b	#0xFF,P2OUT		; All pins set to low output. Place breakpoint
-		bis.b	#01000001b,P1DIR	; P1.0 and P1.6 as output
-		bic.b	#11111111b,P1OUT	; Clear output in P1
-		bis.b	#00001000b,P1REN	; Select internal Resistor
-		bis.b	#00001000b,P1OUT	; Make it pull up	
-		bis.b	#00001000b,P1IE		; enable P1.3 int.
-		eint				; global interrup enable	
-		mov.w	#0,R7	
+LOW		bic.b	#0xFF,P2OUT		; P2.0-P2.7 as low output
+		bis.b	#11110111b,P1DIR	; P1.0-P1.7 as output ports
+		bic.b	#11110111b,P1OUT	; Clear output in P1
+		bis.b	#00001000b,P1REN	; Select Internal Resistor
+		bis.b	#00001000b,P1OUT	; Make it Pull-Up	
+		bis.b	#00001000b,P1IE		; Enable P1.3 interrupt
+		eint				; global interrupt enable	
+		mov.w	#0,R7			; Interrupt Counter	
 ;--------------------Loop Waiting for player------------------------
-Loop		mov	#30000,R4		; Delay
+Loop		mov	#30000,R4		; Delay for flashing LEDs
 		xor.b	#0xFF,P2OUT		; Toggle LEDs
 DELAY		cmp	#1,R7			; Check if the player pressed button in P1.3
 		jz	OutOfInterrupt		; Start Game
 		add.w	#1,0200h		; Value use for choosing random polynomial 
 		add.w	#1,0202h		; Value use for choosing random X value
 		dec	R4			; Decrement Delay
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		jnz	DELAY
-		jmp	Loop
+		nop				; 1 Clock Cycle Delay
+		nop				; 1 Clock Cycle Delay
+		nop				; 1 Clock Cycle Delay
+		nop				; 1 Clock Cycle Delay
+		nop				; 1 Clock Cycle Delay
+		nop				; 1 Clock Cycle Delay
+		nop				; 1 Clock Cycle Delay
+		nop				; 1 Clock Cycle Delay
+		nop				; 1 Clock Cycle Delay
+		jnz	DELAY			; if not zero, repeate DELAY again
+		jmp	Loop			; else, repeate Loop again
 OutOfInterrupt	bic.b	#0xFF,P2OUT		; Turn off LEDs		
-;----------------Chossing the polynomial------------
-		cmp	#0,R8			; Check wich polynomial was chosen randomly
-		jnz	Next			; Save the address of the first coeficient 
-		mov.w	#P0,R5			; in R5
-		jmp	Out
-Next		cmp	#1,R8
+;----------------Chossing the polynomial------------ Check which polynomial was chosen randomly
+		cmp	#0,R8			; Check if random number is 0
+		jnz	Next			; If not zero keep checking
+		mov.w	#P0,R5			;  Save the address of the first coeficient in R5
+		jmp	Out			
+Next		cmp	#1,R8			; Check if random number is 1
 		jnz	Next1
-		mov.w	#P1,R5
+		mov.w	#P1,R5			; Save the address of the first coeficient 
 		jmp	Out
-Next1		cmp	#2,R8
+Next1		cmp	#2,R8			; Check if random number is 2
 		jnz	Next2
-		mov.w	#P2,R5
+		mov.w	#P2,R5			; Save the address of the first coeficient 
 		jmp	Out
 Next2		mov.w	#P3,R5			; by default is P3,  Address of P is located in R5
-;-----------------Chossing the X Value--------------
-Out		cmp	#0,R6			; Check wich X value was chosen randomly
-		jnz	Next3			; Save the number in R9
-		mov.w	X0,R9
+;-----------------Chossing the X Value-------------- Check wich X value was chosen randomly
+Out		cmp	#0,R6			; Check if random number is 0
+		jnz	Next3			; If not zero keep checking
+		mov.w	X0,R9			; Save the address of the first coeficient in R9
 		jmp	Out1
-Next3		cmp	#1,R6
+Next3		cmp	#1,R6			; Check if random number is 1
 		jnz	Next4
-		mov.w	X1,R9
+		mov.w	X1,R9			; Save the address of the first coeficient 
 		jmp	Out1
-Next4		cmp	#2,R6
+Next4		cmp	#2,R6			; Check if random number is 2
 		jnz	Next5
-		mov.w	X2,R9
+		mov.w	X2,R9			; Save the address of the first coeficient 
 		jmp	Out1
 Next5		mov.w	X3,R9			; by default is X3
 
 ;---X value is located in R9 and the address of the polynomial in R5----
-
 ;-------------------------------------------------------------------------
 ;			Syntethic Division
 ;-------------------------------------------------------------------------
 Out1		mov.w	@R5+,R4			; Initialize the first coeficient and update address of the next coeficient (in R5)
 Continue	Mult	R4,R9			; Mult the first coeficient with X
 		cmp	#1,R11			; Compare if result is out of range
-		jz	Lose
+		jz	Lose			; If out of range, lose
 		mov.w	R15,R4			; Move the Result of the Mult in R4
 		add.w	@R5+,R4			; add the next coeficient to x	
 		Mult 	R4,R9			; Mult the result with x
 		cmp	#1,R11			; Compare if result is out of range
-		jz	Lose
+		jz	Lose			; If out of range, lose
 		mov.w	R15,R4			; Move the Result of the Mult in R4
 		add.w	@R5+,R4			; add the next coeficient to x
 		Mult	R4,R9			; Mult the result with x
 		cmp	#1,R11			; Compare if result is out of range
-		jz	Lose
+		jz	Lose			; If out of range, lose
 		mov.w	R15,R4			; Move the Result of the Mult in R4
 		add.w	@R5+,R4			; add the last coeficient to x
-		jc	Lose
 		
 ;----------------Player Wins-----------------------------------
 		bis.b	#01000000b,P1OUT	; Player Wins, green LED on
-		mov	#0,R6			
+		mov	#0,R6			; Initialize R6
 		mov.w	R4,R5			; Copy result in R5
 		clrc
 		rla	R5			; Extract MSB from R5 to R6
@@ -183,15 +182,15 @@ Continue	Mult	R4,R9			; Mult the first coeficient with X
 		rra	R5
 		rra	R5
 		bis.b	R5,P2OUT		; Copy LSB in Port 2, LEDs in BreadBoard will show the LSB
-		Delay2
-		Delay2
-		Delay2
-		Delay2
-		Delay2
-		Delay2
-		Delay2
+		Delay2	#50000			; Delay 
+		Delay2	#50000			; This delay will give us time to write down the LSB
+		Delay2	#50000
+		Delay2	#50000
+		Delay2	#50000
+		Delay2	#50000
+		Delay2	#50000
 		bic.b	#0xFF,P2OUT		; Turn off LEDs
-		Delay2
+		Delay2	#30000	
 		bis.b	R6,P2OUT		; Copy MSB in Port 2, LEDs in BreadBoard will show the LSB
 		jmp	$
 		
@@ -212,43 +211,43 @@ Check2		bit.w	#1000000000000000b,R14	; Checking sign of R9
 		inv	R14			
 		add.w	#1,R14			; 2's complement of R9
 		add.w	#1,R12			; Counter for negatives
-Return		cmp.w	#2,R12
-		jnz	Return1
-		mov.w	#0,R12
+Return		cmp.w	#2,R12			; Two negative is the same as no negatives
+		jnz	Return1	
+		mov.w	#0,R12			; Easier to compare later in the Mult MACRO
 Return1		ret
 ;-------------------------------------------------------------
-;		P1.3 Interrup Service Routine
+;		P1.3 Interrupt Service Routine
 ;-------------------------------------------------------------
 PBISR		bic.b	#00001000b,P1IFG	; clear int. flag
 		cmp	#1,R7			; Check if it was the second time to press the button in P1.3
 		jz	Restart			; If true jump 
 		mov.w	#1,R7			; (True) Use for checking how many pushes have been made
-		mov.w	#0,R4
-		mov.w	#0,R8
-		mov.w	#0,R6
-;------------------Random Algorithm------------------------------
-First		cmp	#2,R4			; Loop done twice?
-		jz	Exit			; If true Exit
-		rra	&0200h			; Move least significant bit to carry
+		mov.w	#0,R4			; initialize random selection counter
+		mov.w	#0,R8			; Random number for Polynomial selection
+		mov.w	#0,R6			; Random number for value selection
+;------------------Random Algorithm----------------------------------------------------------------------------
+First		cmp	#2,R4			; if loop done twice
+		jz	Exit			; exit
+		rra	&0200h			; else, Move least significant bit to carry
 		rlc	R8			; Roll left the carry to R8
-		inc	R4			; R8 will have the posibility of having a number from
-		jmp	First			; 0 to 3
-Exit		mov.w	#0,R4			; initialize counter
-Second		cmp	#2,R4			; Loop done twice?
-		jz	Exit2			; If true Exit
-		rra	&0202h			; Move least significant bit to carry
+		inc	R4			; increment R4 by one, R8 will have the posibility of having a number from 0 to 3
+		jmp	First			; repeat again
+Exit		mov.w	#0,R4			; initialize random selection counter
+Second		cmp	#2,R4			; if loop done twice
+		jz	Exit2			; Exit
+		rra	&0202h			; else, Move least significant bit to carry
 		rlc	R6			; Roll left the carry to R6
-		inc	R4			
-		jmp	Second
-Restart		bic.b	#11111111b,P2OUT	; Turn off all LEDs 
-		bic.b	#11111111b,P1OUT
+		inc	R4			; increment R4 by one, R6 will have the posibility of having a number from 0 to 3	
+		jmp	Second			; repeat loop
+Restart		bic.b	#11111111b,P2OUT	; Turn off all Port 2 LEDs 
+		bic.b	#11111111b,P1OUT	; Turn off all Port 1 LEDs
 		bis.w	#CPUOFF +GIE,SR		; Sleep CPU
 Exit2		reti 				; return from ISR
-;--------------------------------------------------
+;---------------------------------------------------------------------------------------------------------------
 ;	Interrrupt Vectors
-;-------------------------------------------------
+;---------------------------------------------------------------------------------------------------------------
 		ORG	0FFFEh			; MSP430 Reset Vector
 		DW	RESET			;
-		ORG	0FFE4h		; interrupt vector 2
-		DW	PBISR		; address of label PBISR
+		ORG	0FFE4h			; interrupt vector 2
+		DW	PBISR			; address of label PBISR
 		END
